634b1962ffc25459286310d657538fc24d1d5c0f
shelldan <shelldanlee@gmail.com>
2022-11-07 13:30:14 -0600

    update variable match to the model on profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,14 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#project-name').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
+    const title = document.querySelector('#project-name').value.trim();
+    const content = document.querySelector('#project-desc').value.trim();
 
-    if(name && description) {
+    if(title && content) {
         //Send a POST request to the API endpoint
         const response = await fetch(`/api/posts`, {
             method: 'POST',
-            body: JSON.stringify({ name, description }),
+            body: JSON.stringify({ title, content }),
             headers: {
                 'Content-Type': 'application/json',
             },
